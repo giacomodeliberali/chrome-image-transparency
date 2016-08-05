3898984487c0a200a8f52ff738ab968d079bf566
Tomek Fijoł <tomekfijol@gmail.com>
2016-08-05 12:19:52 +0200

    Update options.js

--- options.js
+++ options.js
@@ -2,7 +2,7 @@
 
 (function(glob) {
 
-  var
+  let
     extension = chrome.runtime.getManifest(),
     log = console.log.bind(
       console,
@@ -63,68 +63,47 @@
   }
 
   function restoreValues() {
-
     color1.value = getValue('color1');
     color2.value = getValue('color2');
     size.value = getValue('size');
-
     getTemplateAndSetIt();
-
     syncSize.call(size);
-
   }
 
   function resetValues() {
-
     saveValue('color1', defaults.color1, true);
     saveValue('color2', defaults.color2, true);
     saveValue('size', defaults.size);
-
     restoreValues();
-
   }
 
   function setEvents() {
-
     color1.addEventListener('input', debounce(function() {
       saveValue('color1', this.value);
     }, 500));
-
     color2.addEventListener('input', debounce(function() {
       saveValue('color2', this.value);
     }, 500));
-
     size.addEventListener('input', syncSize);
-
     size.addEventListener('input', debounce(function() {
       saveValue('size', this.value);
     }, 500));
-
     reset.addEventListener('click', resetValues);
-
   }
 
   function setDefaultsAndStart(response) {
-
     if (response && response.defaults) {
-
       defaults = response.defaults;
-
       setEvents();
-
       restoreValues();
-
     }
-
   }
 
   function init() {
-
     // defaults in one place
     chrome.runtime.sendMessage({
       action: 'giveDefaults'
     }, setDefaultsAndStart);
-
   }
 
   doc.addEventListener('DOMContentLoaded', init);
